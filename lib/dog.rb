3a6902646e7b522cc89cfa58735841ea891f735d
kamausuzan197 <kamaususan197@gmail.com>
2022-08-17 19:20:53 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,41 @@
 class Dog
+    def name=(name)
+     @name = name
+    end
 
+     def name 
+        @name
+     end
+     
+     def breed=(breed)
+      @breed=breed
+  end
+  def breed
+      @breed
+  end
 end
+
+lassie = Dog.new
+lassie.name="Lassie"
+
+puts lassie.name
+
+
+class Dog
+   def name=(name)
+       @name=name
+   end
+   def name
+       @name
+   end
+   def breed=(breed)
+       @breed=breed
+   end
+   def breed
+       @breed
+   end
+end
+
+suzan = Dog.new
+suzan.name="suzan"
+puts suzan.name
